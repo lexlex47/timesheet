959dd5156d481b6f76f2d534765289368d4699d9
lexlex47 <lixianglex0727@live.com>
2021-04-05 18:47:59 +1000

    implemnet controller and views

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -1,3 +1,42 @@
 class EntriesController < ApplicationController
+
+  def index
+    @entries = Entry.order('created_at DESC')
+  end
+
+  def new
+    @entry = Entry.new 
+  end
+
+  def create
+    @entry = Entry.new(entry_params)
+    if @entry.save
+      flash[:notice] = "Timesheet Entry create successful"
+      redirect_to root_path
+    else
+      flash[:alert] = @entry.errors.full_messages
+      redirect_to new_entry_path
+    end
+  end
+
+  def update
+    @entry = Entry.find(params[:id])
+    if @entry.save
+      flash[:notice] = "Timesheet Enrty update successful"
+    else
+      flash[:alert] = @entry.errors.full_messages
+    end
+    redirect_to root_path
+  end
+
+  def destroy
+
+  end
+
+  private
+
+  def entry_params
+    params.require(:entry).permit(:date, :start_time, :finish_time)
+  end
   
 end
